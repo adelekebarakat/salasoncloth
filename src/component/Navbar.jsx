0c980f8ecd89f6d130cdfb116e928be4209c65ed
Adeleke Barakat <adelekebarakat96@gmail.com>
2023-08-02 13:43:01 +0100

    commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,13 +4,12 @@ const NavBar = () =>{
     return(
         <Navbar collapseOnSelect expand="lg" bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">TemmyStore</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              
+              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
@@ -20,10 +19,13 @@ const NavBar = () =>{
                 <NavDropdown.Item href="#action/3.4">
                   Separated link
                 </NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Contact</Nav.Link>
+              <Nav.Link href="#deets">Gallery</Nav.Link>
               <Nav.Link eventKey={2} href="#memes">
                 Dank memes
               </Nav.Link>
